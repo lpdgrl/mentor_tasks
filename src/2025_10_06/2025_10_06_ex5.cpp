fd391e2f3c16e4a383c0d6c89ebaa9fd5a9c2a23
lpdgrl <leopard.grellii@gmail.com>
2025-10-09 00:39:18 +0500

    Add decision 2025_10_06_ex5.cpp

---
+++ src/2025_10_06/2025_10_06_ex5.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <unordered_set>
+#include <string_view>
+
+bool is_spec_ch(char c) {
+    static constexpr std::string_view specs = ":;<=>?!\"$%&'()*+-,./ ";
+    return specs.find(c) != std::string_view::npos;
+}
+
+bool is_palindrome(const std::string& str) {
+    if (str.empty()) {
+        return true;
+    }
+
+    size_t left = 0, right = str.size() - 1;
+    while (left < right) {
+        while (left < right && is_spec_ch(str[left])) {
+            ++left;
+        }
+        while (left < right && is_spec_ch(str[right])) {
+            --right;
+        }
+
+        if (left >= right) { 
+            break;
+        }
+
+        if (std::tolower(str[left++]) != std::tolower(str[right--])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void tests() {
+      // Простые положительные случаи
+      assert(is_palindrome("mom"));
+      assert(is_palindrome("dad"));
+      assert(is_palindrome("level"));
+      assert(is_palindrome("a"));
+      assert(is_palindrome("aa"));
+      assert(is_palindrome("aabaa"));
+  
+      // Простые отрицательные случаи
+      assert(!is_palindrome("ab"));
+      assert(!is_palindrome("hello"));
+      assert(!is_palindrome("aab"));
+      assert(!is_palindrome("palindrome"));
+  
+      // Смешанный регистр
+      assert(is_palindrome("Taco cat"));
+      assert(is_palindrome("Noon"));
+      assert(is_palindrome("MadAm"));
+      assert(is_palindrome("RaceCar"));
+  
+      // С пробелами и пунктуацией
+      assert(is_palindrome("A man, a plan, a canal: Panama"));
+      assert(is_palindrome("Go deliver a dare vile dog"));
+      assert(!is_palindrome("race a car"));
+      assert(is_palindrome("'Never odd or even'"));
+      assert(is_palindrome("Was it a car or a cat I saw?"));
+      assert(is_palindrome("Murder for a jar of red rum!"));
+  
+      // С числами и символами
+      assert(is_palindrome("12321"));
+      assert(!is_palindrome("123421"));
+      assert(is_palindrome("1a2b2a1"));
+}
+
+
+int main() {
+    tests();
+    return EXIT_SUCCESS;
+}
