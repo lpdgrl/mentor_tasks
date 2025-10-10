421a7eb4a9c3cadf207f1f4dc2226e1e4527243e
lpdgrl <leopard.grellii@gmail.com>
2025-10-11 00:41:20 +0500

    Decision expanding from center str 2025_10_06_ex5.cpp

--- src/2025_10_06/2025_10_06_ex5.cpp
+++ src/2025_10_06/2025_10_06_ex5.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
 #include <cassert>
-#include <unordered_set>
 #include <string_view>
+#include <ranges>
 
 bool is_spec_ch(char c) {
     static constexpr std::string_view specs = ":;<=>?!\"$%&'()*+-,./ ";
@@ -14,20 +14,18 @@ bool is_palindrome(const std::string& str) {
         return true;
     }
 
-    size_t left = 0, right = str.size() - 1;
-    while (left < right) {
-        while (left < right && is_spec_ch(str[left])) {
-            ++left;
-        }
-        while (left < right && is_spec_ch(str[right])) {
-            --right;
-        }
+    // Just new str from filtered ranges. This feature is new to me :)
+    std::string str_ = str
+                       | std::ranges::views::filter([](char ch) { return !is_spec_ch(ch); })
+                       | std::ranges::to<std::string>();
 
-        if (left >= right) { 
-            break;
-        }
+    int center = str_.size() / 2;
+    bool odd = (str_.size() % 2) != 0 ? true : false;
+    int left = odd ? center : center - 1;
+    int right = center;
 
-        if (std::tolower(str[left++]) != std::tolower(str[right--])) {
+    while (left >= 0 && right < str.size()) {
+        if (std::tolower(str_[left--]) != std::tolower(str_[right++])) {
             return false;
         }
     }
@@ -36,38 +34,39 @@ bool is_palindrome(const std::string& str) {
 }
 
 void tests() {
-      // Простые положительные случаи
-      assert(is_palindrome("mom"));
-      assert(is_palindrome("dad"));
-      assert(is_palindrome("level"));
-      assert(is_palindrome("a"));
-      assert(is_palindrome("aa"));
-      assert(is_palindrome("aabaa"));
+    // Простые положительные случаи
+    assert(is_palindrome("mom"));
+    assert(is_palindrome("dad"));
+    assert(is_palindrome("level"));
+    assert(is_palindrome("a"));
+    assert(is_palindrome("aa"));
+    assert(is_palindrome("aabaa"));
   
-      // Простые отрицательные случаи
-      assert(!is_palindrome("ab"));
-      assert(!is_palindrome("hello"));
-      assert(!is_palindrome("aab"));
-      assert(!is_palindrome("palindrome"));
-  
-      // Смешанный регистр
-      assert(is_palindrome("Taco cat"));
-      assert(is_palindrome("Noon"));
-      assert(is_palindrome("MadAm"));
-      assert(is_palindrome("RaceCar"));
-  
-      // С пробелами и пунктуацией
-      assert(is_palindrome("A man, a plan, a canal: Panama"));
-      assert(is_palindrome("Go deliver a dare vile dog"));
-      assert(!is_palindrome("race a car"));
-      assert(is_palindrome("'Never odd or even'"));
-      assert(is_palindrome("Was it a car or a cat I saw?"));
-      assert(is_palindrome("Murder for a jar of red rum!"));
-  
-      // С числами и символами
-      assert(is_palindrome("12321"));
-      assert(!is_palindrome("123421"));
-      assert(is_palindrome("1a2b2a1"));
+    // Простые отрицательные случаи
+    assert(!is_palindrome("ab"));
+    assert(!is_palindrome("hello"));
+    assert(!is_palindrome("aab"));
+    assert(!is_palindrome("palindrome"));
+
+    // Смешанный регистр
+    assert(is_palindrome("Taco cat"));
+    assert(is_palindrome("Noon"));
+    assert(is_palindrome("MadAm"));
+    assert(is_palindrome("RaceCar"));
+
+    // С пробелами и пунктуацией
+    // amanaplanacanalpanama
+    assert(is_palindrome("A man, a plan, a canal: Panama"));
+    assert(is_palindrome("Go deliver a dare vile dog"));
+    assert(!is_palindrome("race a car"));
+    assert(is_palindrome("'Never odd or even'"));
+    assert(is_palindrome("Was it a car or a cat I saw?"));
+    assert(is_palindrome("Murder for a jar of red rum!"));
+
+    // С числами и символами
+    assert(is_palindrome("12321"));
+    assert(!is_palindrome("123421"));
+    assert(is_palindrome("1a2b2a1"));
 }
 
 
